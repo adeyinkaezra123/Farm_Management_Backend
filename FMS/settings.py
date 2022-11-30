7df1a8e351e6c8c42bb25dc0acd7e98623e76513
Oniyide Faith <72167184+oniyidefaith@users.noreply.github.com>
2022-11-30 11:16:29 +0100

    new

--- FMS/settings.py
+++ FMS/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 ALLOWED_HOST = ['*']
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Application definition
